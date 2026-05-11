eb66e3ef5264d981320621172dea8d559c9bf20d
Williams Rodrigues <seu_email_do_github@gmail.com>
2026-05-10 23:30:40 -0300

    Incluindo novas libs ao projeto

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import warnings
+import plotly.express as px
+import plotly.graph_objects as go
 
 warnings.filterwarnings("ignore")
 
